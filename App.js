7591606f662c5452e8df87744b309c3414498d0b
한지희, Jihee Han <hatrix1014@gmail.com>
2023-05-21 20:21:23 +0900

    [Update]레이아웃 설정

--- App.js
+++ App.js
@@ -1,26 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>벌써 재밌다</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>  
+      </View>
+      <ScrollView horizontal contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>  
+          <Text style={styles.description}>화창</Text>  
+        </View>  
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>  
+          <Text style={styles.description}>화창</Text>  
+        </View>  
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>  
+          <Text style={styles.description}>화창</Text>  
+        </View>  
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>  
+          <Text style={styles.description}>화창</Text>  
+        </View>  
+      </ScrollView>         
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1, 
+    backgroundColor:"tomato"
+  },
+  city: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 58,
+    fontWeight: "800",
+  },
+  weather: {
+    flex: 2,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 158,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 100,
   },
 });
-
-
-
 
 
 
